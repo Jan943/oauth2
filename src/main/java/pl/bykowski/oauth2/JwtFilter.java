4ef8c29f2ca963c96007a3faa6e9341f3bb931ea
Jan943 <jansopata10@gmail.com>
2022-07-18 22:57:10 +0200

    Logging with JWT:
    -create POJO Class(User)
    -add necessary class implementing Filter
    -create PostMapping method with logging and returning JWT
    -add jwt and jaxb-api dependency
    -GetMapping method as a resource to access with JWT

---
+++ src/main/java/pl/bykowski/oauth2/JwtFilter.java
@@ -0,0 +1,11 @@
+package pl.bykowski.oauth2;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class JwtFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
