4b04d91a17acf11f265300814747b363d83ab342
Jan943 <jansopata10@gmail.com>
2022-07-25 22:46:00 +0200

    Add validation in Filter

--- src/main/java/pl/bykowski/oauth2/JwtFilter.java
+++ src/main/java/pl/bykowski/oauth2/JwtFilter.java
@@ -1,11 +1,34 @@
 package pl.bykowski.oauth2;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class JwtFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String header = httpServletRequest.getHeader("authorization");
 
+        if(httpServletRequest == null || !header.startsWith("Bearer "))
+        {
+            throw new ServletException("Wrong or empty header");
+        }
+        else
+        {
+            try {
+                String token = header.substring(7);
+                Claims claims = Jwts.parser().setSigningKey("jan123").parseClaimsJws(token).getBody();
+                servletRequest.setAttribute("claims", claims);
+            }
+            catch (Exception e)
+            {
+                throw new ServletException("Wrong key");
+            }
+        }
+        filterChain.doFilter(servletRequest,servletResponse);
     }
 }
